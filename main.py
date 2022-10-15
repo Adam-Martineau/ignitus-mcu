f4c31324db938a00d0923d41a4eb2a1d4c4faf7e
Adam-Martineau <83926133+Adam-Martineau@users.noreply.github.com>
2022-10-15 13:36:24 -0400

    fasdf

--- main.py
+++ main.py
@@ -8,15 +8,20 @@ from machine import Pin, UART, RTC, I2C, SoftI2C, PWM, SoftSPI, Timer
 ############### CONSTANTS #################
 ###########################################
 
+multiplexor = false
+
 _serial_start = "_JSONHEADER_"
 _serial_stop = "_JSONFOOTER_"
+
+# Purge valve pin
+purge_valve_pin = 3
 
 # UART pins
 uart_tx = 0
 uart_rx = 1
 
 # Servo motor
-servo_pin = 13
+servo_pin = 6
 
 # SPI pinout for the uSD card reader
 uSD_miso = 20
@@ -29,16 +34,19 @@ i2c_sda = 4
 i2c_scl = 5
 
 # i2c addresses for the m3200 sensors
-m32_multiplexer = 0x28
-m32_tank1 = b'1'
-m32_tank2 = b'2'
-m32_engine = b'3'
+m32_multiplexer = 0x70
+m32_tank1 = b'2'
+m32_tank2 = b'4'
+m32_engine = b'6'
 
 # i2c param for the gps
-gps_add = 0x42
+gps_add = 66
+
+ignition = 13
+continuite = 9
 
 # i2c param for the current sensor
-current_sensor_add = 0x40
+current_sensor_add = 64
 current_sensor_reg_config = 0x00
 current_sensor_reg_shunt_voltage = 0x01
 current_sensor_reg_bus_voltage = 0x02
@@ -48,211 +56,8 @@ current_sensor_reg_cal = 0x05
 max_current = 2
 r_shunt = 0.1
 
+uart = UART(0, baudrate=9600, tx=Pin(uart_tx), rx=Pin(uart_rx), bits=8, parity=None, stop=1)
 
-###########################################
-################## GPS ####################
-###########################################
-
-class gps_data:
-    add:int = 0
-    n_sat = 0 
-    long = 0
-    lat = 0
-    
-    def __str__(self):
-        return json.dumps(
-            {
-                "add": self.add,
-                "n_sat": self.n_sat,
-                "long": self.long,
-                "lat": self.lat,
-            }
-        )
-
-class gps_sensor:
-    data = gps_data()
-    
-    def __init__(self, add:int):
-        self.sda = i2c_sda
-        self.scl = i2c_scl
-        self.add = add
-
-        self.i2c = SoftI2C(
-            scl=Pin(self.scl), 
-            sda=Pin(self.sda), 
-            freq=100_000)
-
-    def get_data(self):
-        n_bytes = self._read_nbr_availlable_bytes()
-        raw_data = self._read_bytes(n_bytes)
-        self._convert_data(raw_data)
-        self.data.add = self.add
-        return self.data
-
-    def _read_nbr_availlable_bytes(self) -> int:
-        n_bytes_msb = int.from_bytes(self.i2c.readfrom_mem(self.add, 0xFD, 1), "big")
-        n_bytes_lsb = int.from_bytes(self.i2c.readfrom_mem(self.add, 0xFE, 1), "big")
-        return (n_bytes_msb << 8) + n_bytes_lsb
-
-    def _read_bytes(self, n_bytes):
-        print(n_bytes)
-        if not n_bytes == 0:
-            #return self.i2c.readfrom_mem(self.add, 0xFF, n_bytes)
-            data = ""
-            while not data == 0xff:
-                data = self.i2c.readfrom(self.add, 1).decode("utf-8")
-                print(data)
-
-    def _convert_data(self, raw_data):
-        pass
-
-
-###########################################
-############## SERVO MOTOR ################
-###########################################
-
-pwm_min = 21845
-pwm_max = 43690
-
-class servo:
-    def __init__(self, pin):
-        self.pin = Pin(pin)
-        self.pwm = PWM(self.pin)
-        self.pwm.freq(333)
-
-    def open(self):
-        self.pwm.duty_u16(43690)
-    
-    def middle(self):
-        self.pwm.duty_u16(32767)
-
-    def close(self):
-        self.pwm.duty_u16(21845)
-        
-    def set_percent(self, percent):
-        range = pwm_max - pwm_min
-        value = (percent * range) // 100
-        value = value + pwm_min
-        self.pwm.duty_u16(value)
-
-
-###########################################
-############# POWER SENSOR ################
-###########################################
-
-class power_supplie_data:
-    add: int = 0
-    power = 0
-    current = 0
-    voltage = 0
-
-class power_supplie_sensor:
-    data = power_supplie_data()
-        
-    def __init__(self, add:int):
-        self.sda = i2c_sda
-        self.scl = i2c_scl
-        self.add = current_sensor_add
-        self.current_lsb = max_current/(2**15)
-        
-        self.data.add = self.add
-
-        self.i2c = SoftI2C(
-            scl=Pin(self.scl), 
-            sda=Pin(self.sda), 
-            freq=100_000)
-    
-    def get_data(self) -> power_supplie_data:
-        self._write_calibration()
-        
-        self._read_voltage_bytes()
-        self._read_current_bytes()
-        self._read_power_bytes()
-        
-        self._convert_voltage()
-        self._convert_current()
-        self._convert_power()
-        
-        return self.data
-
-    def _convert_current(self):
-        self.data.current = self.data.current * self.current_lsb
-        
-    def _convert_power(self):
-        self.data.power = self.data.power * (self.current_lsb * 20)
-
-    def _convert_voltage(self):
-        self.data.voltage = self.data.voltage >> 3
-        self.data.voltage = self.data.voltage * 0.004
-
-    def _write_calibration(self):
-        reg = current_sensor_reg_cal
-        cal = self._get_cal()
-        self.i2c.writeto_mem(self.add, reg, cal)
-
-    def _read_current_bytes(self):
-        reg = current_sensor_reg_current
-        self.data.current = int.from_bytes(self.i2c.readfrom_mem(self.add, reg, 2), "big")
-
-    def _read_power_bytes(self):
-        reg = current_sensor_reg_power
-        self.data.power = int.from_bytes(self.i2c.readfrom_mem(self.add, reg, 2), "big")
-
-    def _read_voltage_bytes(self):
-        reg = current_sensor_reg_bus_voltage
-        self.data.voltage = int.from_bytes(self.i2c.readfrom_mem(self.add, reg, 2), "big")
-
-    def _get_cal(self):
-        value = trunc(0.04096 / (self.current_lsb * r_shunt))
-        return bytearray(value.to_bytes(2, "big"))
-
-
-###########################################
-############## M32 SENSOR #################
-###########################################
-
-class m32_data:
-    add: int = None
-    temp = None
-    status: int = None
-    pressure = None
-    
-class m32_sensor:
-    data = m32_data()
-    
-    def __init__(self, add):
-        self.sda = i2c_sda
-        self.scl = i2c_scl
-        self.add = add
-        
-        self.data.add = self.add
-
-        self.i2c = I2C(0,
-            scl=Pin(self.scl), 
-            sda=Pin(self.sda), 
-            freq=100_000)
-
-    def get_data(self):
-        self._read_bytes()
-        self._convert_press()
-        self._convert_temp()
-        
-        return self.data
-
-    def _convert_temp(self):
-        self.data.temp = (5370619521453261/54975581388800000) * self.data.temp - (25009/500)
-
-    def _convert_press(self):
-        print(self.data.pressure)
-        self.data.pressure = (785370160604119/109951162777600000) * self.data.pressure - (71429/10000)
-        self.data.pressure = (self.data.pressure * 350) / 100
-        
-    def _read_bytes(self):
-        #self.i2c.writeto(self.add, self.add.to_bytes(2, "big"))
-        mybytes = self.i2c.readfrom(self.add, 4, True)
-        self.data.status = mybytes[0] >> 6
-        self.data.pressure = ((mybytes[0] << 8) + mybytes[1]) & 0x3FFF
-        self.data.temp = ((mybytes[2] << 8) + mybytes[3]) >> 5
 
 ###########################################
 ################ SD CARD ##################
@@ -514,21 +319,212 @@ class SDCard:
         if op == 4:  # get number of blocks
             return self.sectors
 
+###########################################
+################## GPS ####################
+###########################################
+
+class gps_data:
+    add:int = 0
+    n_sat = 0 
+    long = 0
+    lat = 0
+    
+    def __str__(self):
+        return json.dumps(
+            {
+                "add": self.add,
+                "n_sat": self.n_sat,
+                "long": self.long,
+                "lat": self.lat,
+            }
+        )
+
+class gps_sensor:
+    data = gps_data()
+    
+    def __init__(self, add:int):
+        self.sda = i2c_sda
+        self.scl = i2c_scl
+        self.add = add
+
+        self.i2c = SoftI2C(
+            scl=Pin(self.scl), 
+            sda=Pin(self.sda), 
+            freq=100_000)
+
+    def get_data(self):
+        n_bytes = self._read_nbr_availlable_bytes()
+        raw_data = self._read_bytes(n_bytes)
+        self._convert_data(raw_data)
+        self.data.add = self.add
+        return self.data
+
+    def _read_nbr_availlable_bytes(self) -> int:
+        n_bytes_msb = int.from_bytes(self.i2c.readfrom_mem(self.add, 0xFD, 1), "big")
+        n_bytes_lsb = int.from_bytes(self.i2c.readfrom_mem(self.add, 0xFE, 1), "big")
+        return (n_bytes_msb << 8) + n_bytes_lsb
+
+    def _read_bytes(self, n_bytes):
+        print(n_bytes)
+        if not n_bytes == 0:
+            #return self.i2c.readfrom_mem(self.add, 0xFF, n_bytes)
+            data = ""
+            while not data == 0xff:
+                data = self.i2c.readfrom(self.add, 1).decode("utf-8")
+                print(data)
+
+    def _convert_data(self, raw_data):
+        pass
+
+
+###########################################
+############## SERVO MOTOR ################
+###########################################
+
+pwm_min = 21845
+pwm_max = 43690
+
+class servo:
+    def __init__(self, pin):
+        self.pin = Pin(pin)
+        self.pwm = PWM(self.pin)
+        self.pwm.freq(333)
+
+    def open(self):
+        self.pwm.duty_u16(43690)
+    
+    def middle(self):
+        self.pwm.duty_u16(32767)
+
+    def close(self):
+        self.pwm.duty_u16(21845)
+        
+    def set_percent(self, percent):
+        range = pwm_max - pwm_min
+        value = (percent * range) // 100
+        value = value + pwm_min
+        self.pwm.duty_u16(value)
+
+
+###########################################
+############# POWER SENSOR ################
+###########################################
+
+class power_supplie_data:
+    add = 0
+    power = 0
+    current = 0
+    voltage = 0
+
+class power_supplie_sensor:
+    data = power_supplie_data()
+    current_lsb = max_current/(2**15)
+    
+    def get_data(self) -> power_supplie_data:
+        self.data.add = current_sensor_add
+        return self.data
+    
+        self._write_calibration()
+        
+        self._read_voltage_bytes()
+        self._read_current_bytes()
+        self._read_power_bytes()
+        
+        self._convert_voltage()
+        self._convert_current()
+        self._convert_power()
+        
+        return self.data
+
+    def _convert_current(self):
+        self.data.current = self.data.current * self.current_lsb
+        
+    def _convert_power(self):
+        self.data.power = self.data.power * (self.current_lsb * 20)
+
+    def _convert_voltage(self):
+        self.data.voltage = self.data.voltage >> 3
+        self.data.voltage = self.data.voltage * 0.004
+
+    def _write_calibration(self):
+        i2c = I2C(0, scl=Pin(i2c_scl), sda=Pin(i2c_sda), freq=100_000)
+        reg = current_sensor_reg_cal
+        cal = self._get_cal()
+        i2c.writeto_mem(current_sensor_add, reg, cal)
+
+    def _read_current_bytes(self):
+        i2c = I2C(0, scl=Pin(i2c_scl), sda=Pin(i2c_sda), freq=100_000)
+        reg = current_sensor_reg_current
+        self.data.current = int.from_bytes(i2c.readfrom_mem(current_sensor_add, reg, 2), "big")
+
+    def _read_power_bytes(self):
+        i2c = I2C(0, scl=Pin(i2c_scl), sda=Pin(i2c_sda), freq=100_000)
+        reg = current_sensor_reg_power
+        self.data.power = int.from_bytes(i2c.readfrom_mem(current_sensor_add, reg, 2), "big")
+
+    def _read_voltage_bytes(self):
+        i2c = I2C(0, scl=Pin(i2c_scl), sda=Pin(i2c_sda), freq=100_000)
+        reg = current_sensor_reg_bus_voltage
+        self.data.voltage = int.from_bytes(i2c.readfrom_mem(current_sensor_add, reg, 2), "big")
+
+    def _get_cal(self):
+        value = trunc(0.04096 / (self.current_lsb * r_shunt))
+        return bytearray(value.to_bytes(2, "big"))
+
+
+###########################################
+############## M32 SENSOR #################
+###########################################
+
+class m32_data:
+    add = 0
+    temp = 0
+    status = 0
+    pressure = 0
+    
+class m32_sensor:
+    data = m32_data()
+
+    def get_data(self):
+        self.data.add = m32_multiplexer
+        
+        self._read_bytes()
+        self._convert_press()
+        self._convert_temp()
+        return self.data
+
+    def _convert_temp(self):
+        self.data.temp = (5370619521453261/54975581388800000) * self.data.temp - (25009/500)
+
+    def _convert_press(self):
+        self.data.pressure = (785370160604119/109951162777600000) * self.data.pressure - (71429/10000)
+        self.data.pressure = (self.data.pressure * 350) / 100
+        self.data.pressure = self.data.pressure * 14.506
+        
+    def _read_bytes(self):
+        i2c = I2C(0, scl=Pin(i2c_scl), sda=Pin(i2c_sda), freq=100_000)
+        i2c.writeto(m32_multiplexer, m32_multiplexer.to_bytes(2, 'big'))
+        mybytes = i2c.readfrom(m32_multiplexer, 4, True)
+
+        self.data.status = mybytes[0] >> 6
+        self.data.pressure = ((mybytes[0] << 8) + mybytes[1]) & 0x3FFF
+        self.data.temp = ((mybytes[2] << 8) + mybytes[3]) >> 5
+    
 
 ###########################################
 ############### system_data ###############
 ###########################################
 
 class system_data:
-    tank1:m32_data = None
-    tank2:m32_data = None
-    engine:m32_data = None
-    power:power_supplie_data = None
-    battery:bool = False
-    armed:bool = False
-    ignition:bool = False
-    purge_valve:bool = False
-    main_valve:bool = False
+    tank1 = m32_data()
+    tank2 = m32_data()
+    engine = m32_data()
+    power = power_supplie_data()
+    battery = False
+    armed = False
+    ignition = False
+    purge_valve = False
+    main_valve = False
 
     def __str__(self):
         return json.dumps(
@@ -542,7 +538,7 @@ class system_data:
                 'tank2_status': self.tank2.status,
                 'tank2_pressure': self.tank2.pressure,
                 'engine_add': self.engine.add,
-                'engine_temp': self.engine.add,
+                'engine_temp': self.engine.temp,
                 'engine_status': self.engine.status,
                 'engine_pressure': self.engine.pressure,
                 'power_add': self.power.add,
@@ -557,13 +553,7 @@ class system_data:
             }
         )
         
-
-# main data struc
 data = system_data()
-
-# sensors definitions
-tank1_sensor = m32_sensor(m32_multiplexer)
-power_sensor = power_supplie_sensor(gps_add)
 
 
 ###########################################
@@ -576,8 +566,9 @@ class logger:
         self.uart = uart
         
     def write(self):
+        print(data.tank1.pressure)
         self._write_to_serial(_serial_start + str(data) + _serial_stop)
-        #self._write_to_uSD(str(data))
+        self._write_to_uSD(str(data))
     
     def _write_to_serial(self, msg: str):
         self.uart.write(msg)
@@ -611,25 +602,32 @@ class logger:
         # Create a file and write something to it
         with open(path, "a") as file:
             print(msg, file=file)
+            
+log = logger("test", uart)
+
 
 ###########################################
 ################ CONTROL ##################
 ###########################################
 
 def purge_open():
-    pass
+    purge_valve = Pin(purge_valve_pin, Pin.OUT)
+    purge_valve.on()
 
 
 def purge_close():
-    pass  
+    purge_valve = Pin(purge_valve_pin, Pin.OUT)
+    purge_valve.off()
 
 
 def main_valve_open():
-    pass
+    valve = servo(servo_pin)
+    valve.open()
 
 
 def main_valve_close():
-    pass
+    valve = servo(servo_pin)
+    valve.close()
 
 
 def emergency_stop():
@@ -640,14 +638,13 @@ def emergency_stop():
 
 
 def ignition():
-    pass
+    purge_valve = Pin(ignition, Pin.OUT)
+    purge_valve.on()
+
 
 ###########################################
 ################# MAIN ####################
 ###########################################
-
-uart = UART(0, baudrate=9600, tx=Pin(uart_tx), rx=Pin(uart_rx), bits=8, parity=None, stop=1)
-log = logger("test", uart)
 
 def switch_multiplexer(device):
     i2c = I2C(0,
@@ -658,46 +655,25 @@ def switch_multiplexer(device):
     i2c.writeto(m32_multiplexer, device)
 
 def refresh_data(t):
-    m32 = m32_data()
-    m32.add = 32
-    m32.temp = 43
-    m32.status = 2
-    m32.pressure = 134
+        m32 = m32_sensor()
+        power_sensor = power_supplie_sensor()
     
-#    data.tank1 = m32
-    data.tank2 = m32
-    data.engine = m32
+    if(multiplexor)
+        switch_multiplexer(m32_tank1)
+        data.tank1 = m32.get_data()
+        
+        switch_multiplexer(m32_tank2)
+        data.tank2 = m32.get_data()
+    else
+        m32_multiplexer = 40
+        data.tank1 = m32.get_data()
+        data.tank2 = m32_data()
     
-    power = power_supplie_data()
-    m32.add = 32
-    m32.voltage = 43
-    m32.current = 2
-    m32.power = 134
+    #purge_valve = Pin(purge_valve_pin, Pin.IN)
+    #data.purge_valve = purge_valve.value()
     
-    data.power = power
+    data.power = power_sensor.get_data()
 
-    try:
-#        switch_multiplexer(m32_tank1)
-        data.tank1 = tank1_sensor.get_data()
-    except:
-        print("Error reading data from tank1.")
-#    
-#    try:
-#        switch_multiplexer(m32_tank2)
-#        data.tank2 = tank2_sensor.get_data()
-#    except:
-#        print("Error reading data from tank2.")
-#        
-#    try:
-#        switch_multiplexer(m32_engine)
-#        data.engine = engine_sensor.get_data()
-#    except:
-#        print("Error reading data from engine.")
-#        
-#    try:
-#        data.power = power_sensor.get_data()
-#    except:
-#        print("Error reading data from power.")
 
 class serial:
     # Here we read the uart buffer and add any character to uart_rx if we find any command,
@@ -712,7 +688,7 @@ class serial:
                      "purge_close",
                      "data"]
 
-    buffer = "uart buffer:"
+    buffer = ""
     
     def __init__(self, uart):
         self.uart = uart
@@ -728,9 +704,13 @@ class serial:
     
     def write_data():
         uart.write(data)
+        self.buffer = ""
 
     def write(self, out):
         uart.write(out)
+        self.buffer = ""
+        
+serialPort = serial(uart)
 
 def exec_cmd(cmd):
     if cmd == "emergency_stop":
@@ -755,8 +735,6 @@ def exec_cmd(cmd):
         log.write()
 
 
-serialPort = serial(uart)
-
 def commands(t):
     command = serialPort.read()
 
@@ -764,12 +742,21 @@ def commands(t):
         print(command)
         exec_cmd(command)
 
+def init():
+    purge_valve = Pin(ignition, Pin.OUT)
+    purge_valve.off()
+    
+    main_valve_close()
+    purge_close()
+
 def main():
+    init()
+    
     timerRefreshData = Timer()
     timerCommands = Timer()
-
-    timerRefreshData.init(period=250, callback=refresh_data)
-    timerCommands.init(period=25, callback=commands)
+    
+    timerRefreshData.init(period=100, callback=refresh_data)
+    timerCommands.init(period=50, callback=commands)
     
     while True:
         time.sleep(1000)  
--- test.py
+++ test.py
@@ -1,87 +1,92 @@
 # run this file on the rp2040 to use the test function in the
 # python terminal
 
-import constants
-
 from time import sleep_ms,
-from logging import logger
-from servomotor import servo
-from machine import Pin, SoftI2C
-from machine import RTC, Pin, UART
-from m32 import m32_sensor, m32_data
+from machine import RTC, Pin, UART, Pin, SoftI2C
 
 
+###########################################
+############### CONSTANTS #################
+###########################################
+
+# UART pins
+uart_tx = 0
+uart_rx = 1
+
+# Servo motor
+servo_pin = 13
+
+# I2C pinout
+i2c_sda = 4
+i2c_scl = 5
+
+uart = UART(0, baudrate=9600, tx=Pin(uart_tx), rx=Pin(uart_rx), bits=8, parity=None, stop=1)
+
+###########################################
+############## SERVO MOTOR ################
+###########################################
+
+pwm_min = 21845
+pwm_max = 43690
+
+class servo:
+    def __init__(self, pin):
+        self.pin = Pin(pin)
+        self.pwm = PWM(self.pin)
+        self.pwm.freq(333)
+
+    def open(self):
+        self.pwm.duty_u16(43690)
+    
+    def middle(self):
+        self.pwm.duty_u16(32767)
+
+    def close(self):
+        self.pwm.duty_u16(21845)
+        
+    def set_percent(self, percent):
+        range = pwm_max - pwm_min
+        value = (percent * range) // 100
+        value = value + pwm_min
+        self.pwm.duty_u16(value)
+
+
+###########################################
+################# TESTS ###################
+###########################################
+        
 def open_servo():
-    moteur = servo(constants.servo_pin)
+    moteur = servo(servo_pin)
     moteur.open()
 
 def close_servo():
-    moteur = servo(constants.servo_pin)
+    moteur = servo(servo_pin)
     moteur.close()
 
 def servo_percent(p):
-    moteur = servo(constants.servo_pin)
+    moteur = servo(servo_pin)
     moteur.set_percent(p)
 
 def get_gpio(gpio):
     return Pin(gpio).value()
-
-def get_tank1():
-    tank1_sensor = m32_sensor(constants.m32_add_tank_1)
-    return tank1_sensor.get_data()
-
-def get_tank2():
-    tank2_sensor = m32_sensor(constants.m32_add_tank_2)
-    return tank2_sensor.get_data()
-
-def get_engine():
-    engine_sensor = m32_sensor(constants.m32_add_engine)
-    return engine_sensor.get_data()
-
-def get_currant():
-    power_sensor = power_supplie_sensor(constants.gps_add)
-    power_sensor.get_data()
-
-def get_gps():
-    gps_sensor_1 = gps_sensor(constants.gps_add)
-    return gps_sensor_1.get_data()
-    
     
 def i2c_scan():
-    sda = constants.i2c_sda
-    scl = constants.i2c_scl
-    i2c = SoftI2C(scl=Pin(scl), sda=Pin(sda), freq=100_000)
+    i2c = SoftI2C(scl=Pin(i2c_scl), sda=Pin(i2c_sda), freq=100_000)
+    print(i2c.scan())
+    i2c = SoftI2C(scl=Pin(3), sda=Pin(2), freq=100_000)
     print(i2c.scan())
 
 
-def serial_tx(msg):
-    uart = UART(0,
-        baudrate=9600,
-        tx=Pin(constants.uart_tx),
-        rx=Pin(constants.uart_rx),
-        bits=8,
-        parity=None,
-        stop=1)
-    
+def serial_tx(msg):   
     uart.write(msg)
     
 
 def serial_rx():
-    uart = UART(0,
-        baudrate=9600,
-        tx=Pin(constants.uart_tx),
-        rx=Pin(constants.uart_rx),
-        rxbuf = 100,
-        bits=8,
-        parity=None,
-        stop=1)
-    
-    input("Press enter want ready to uart.read()")
     print(uart.read())
     
     
 def logging_test():
-    uart = UART(0, baudrate=9600, tx=Pin(constants.uart_tx), rx=Pin(constants.uart_rx), bits=8, parity=None, stop=1)
+    uart = UART(0, baudrate=9600, tx=Pin(uart_tx), rx=Pin(uart_rx), bits=8, parity=None, stop=1)
     log = logger("thisisatest", uart)
     log.write("test\n\r")
     
